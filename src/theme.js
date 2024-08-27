4558d21ebe96f40e1f6c8c28667a328a789ff758
Dang Xuan Khanh <khanhdang3152@gmail.com>
2024-08-27 21:45:30 +0700

    build basic cleanup in Header component

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,19 @@
 
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 const theme = extendTheme({
+	booking_care: {
+		HEADER_HEIGHT: '78px',
+		WIDTH_COMMON: '1208px',
+	},
 	components: {
 		MuiCssBaseline: {
 			styleOverrides: {
 				html: {},
 				body: {
 					fontSize: '0.875rem',
+					a: {
+						textDecoration: 'none',
+					},
 				},
 			},
 		},
